c16cf97cb44e8e79b5798b3e4cc1228c063a5236
David Strauß <mail@stravid.com>
2014-09-06 13:18:16 +0200

    Add Pikaday and moment.js dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -16,5 +16,8 @@ var app = new EmberAddon();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/momentjs/moment.js');
+app.import('bower_components/pikaday/pikaday.js');
+app.import('bower_components/pikaday/css/pikaday.css');
 
 module.exports = app.toTree();
