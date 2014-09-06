c81b34c5e7322a976a3d2a3eb209bd3ffa7df857
David Strauß <mail@stravid.com>
2014-09-06 14:05:15 +0200

    Try to add bower dependencies

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = {
-  name: 'ember-pikaday'
+  name: 'ember-pikaday',
+  included: function(app) {
+    app.import('bower_components/momentjs/moment.js');
+    app.import('bower_components/pikaday/pikaday.js');
+    app.import('bower_components/pikaday/css/pikaday.css');
+  }
 };
