915d20adcc92b1cf457fbf0a128e82cc1dc2813b
David Strauß <mail@stravid.com>
2014-09-06 21:16:56 +0200

    Use blueprint method for adding dependencies

---
+++ blueprints/ember-pikaday/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    var that = this;
+
+    return this.addBowerPackageToProject('pikaday').then(function() {
+        return that.addBowerPackageToProject('momentjs');
+    });
+  }
+};
