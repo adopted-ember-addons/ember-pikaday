b6762339f53a60a303fb10c90b7ed46a75883df4
David Strauß <david@strauss.io>
2015-06-17 09:34:50 +0200

    Explicitly test certain Ember.js versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,30 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.13.1',
+      dependencies: {
+        'ember': '1.13.1'
+      }
+    },
+    {
+      name: 'ember-1.12.1',
+      dependencies: {
+        'ember': '1.12.1'
+      }
+    },
+    {
+      name: 'ember-1.11.3',
+      dependencies: {
+        'ember': '1.11.3'
+      }
+    },
+    {
+      name: 'ember-1.10.1',
+      dependencies: {
+        'ember': '1.10.1'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
