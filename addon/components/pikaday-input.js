ddc0fa8c140a8b10ff3a520df5ae25457d4f2c5d
leojpod <leo.jpod@gmail.com>
2015-06-21 19:05:03 +0200

    auto hide pikaday calendar when disabled is activated

--- addon/components/pikaday-input.js
+++ addon/components/pikaday-input.js
@@ -78,5 +78,12 @@ export default Ember.Component.extend({
     } else {
       return 10;
     }
-  }
+  },
+
+  autoHideOnDisabled: Ember.observer('disabled', function () {
+    //if disabled is false then hide the pikaday:
+    if (this.get('disabled')) {
+      this.get('pikaday').hide();
+    }
+  })
 });
