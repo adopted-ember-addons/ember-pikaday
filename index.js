b8d67027a2db11aaf04dcef4ca94752860283679
andreas <andreas@agoodid.se>
2015-10-10 22:25:15 +0200

    remove include

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/moment/moment.js');
     app.import(app.bowerDirectory + '/pikaday/pikaday.js');
     app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
   }
