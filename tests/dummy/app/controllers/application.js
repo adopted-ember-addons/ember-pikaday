468a3137d6d0ffea489c8139c8c2fbb72114a391
David Strauß <david@strauss.io>
2015-10-24 16:36:29 +0200

    Make dummy page more useful

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  startDate: new Date()
+  startDate: new Date(),
+  actions: {
+    clearStartDate: function() {
+      this.set('startDate', null);
+    }
+  }
 });
