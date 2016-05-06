0e9c185b8515a7863735b42392ff4a8110bf8140
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-05-06 15:58:12 -0700

    Bump ember-cli-moment-shim

--- blueprints/ember-pikaday/index.js
+++ blueprints/ember-pikaday/index.js
@@ -5,7 +5,7 @@ module.exports = {
     var that = this;
 
     return this.addBowerPackageToProject('pikaday').then(function() {
-      return that.addAddonToProject('ember-cli-moment-shim', '0.6.2');
+      return that.addAddonToProject('ember-cli-moment-shim', '1.1.0');
     });
   }
 };
