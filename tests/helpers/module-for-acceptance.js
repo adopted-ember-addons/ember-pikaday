4c62fabbf26a4fb436350594cf135a6cdc2e6281
Michael Pitzer <mail@mpitzer.at>
2016-09-21 21:24:54 +0200

    Upgrade to ember-cli 2.7.0

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -2,6 +2,7 @@ import { module } from 'qunit';
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
+
 const { RSVP: { Promise } } = Ember;
 
 export default function(name, options = {}) {
