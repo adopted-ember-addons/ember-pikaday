ea20a4fbe4480ea98c804974e09a63e32e222b21
Josemar Luedke <josemarluedke@gmail.com>
2016-10-12 12:27:59 -0700

    Get pikaday from npm instead of Bower

--- blueprints/ember-pikaday/index.js
+++ blueprints/ember-pikaday/index.js
@@ -2,10 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    var that = this;
-
-    return this.addBowerPackageToProject('pikaday').then(function() {
-      return that.addAddonToProject('ember-cli-moment-shim', '1.1.0');
-    });
+    return this.addAddonToProject('ember-cli-moment-shim', '2.1.0');
   }
 };
