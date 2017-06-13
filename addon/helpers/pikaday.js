e916f53ba809e7f4ebe091d05d0490383a994dba
misterAlagiz <alagizov@gmail.com>
2017-06-13 11:11:24 +0200

    distinguish between different pikaday components

--- addon/helpers/pikaday.js
+++ addon/helpers/pikaday.js
@@ -5,46 +5,48 @@ var $ = Ember.$;
 var openDatepicker = function(element) {
   $(element).click();
 
-  return PikadayInteractor;
-};
+  return {
+    selectorForMonthSelect: element + ' .pika-lendar:visible .pika-select-month',
+    selectorForYearSelect: element + ' .pika-lendar:visible .pika-select-year',
+    
+    selectDate: function(date) {
+      var day = date.getDate();
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      var selectEvent = 'ontouchend' in document ? 'touchend' : 'mousedown';
 
-var PikadayInteractor = {
-  selectorForMonthSelect: '.pika-lendar:visible .pika-select-month',
-  selectorForYearSelect: '.pika-lendar:visible .pika-select-year',
-  selectDate: function(date) {
-    var day = date.getDate();
-    var month = date.getMonth();
-    var year = date.getFullYear();
-    var selectEvent = 'ontouchend' in document ? 'touchend' : 'mousedown';
-
-    $(this.selectorForYearSelect).val(year);
-    triggerNativeEvent($(this.selectorForYearSelect)[0], 'change');
-    $(this.selectorForMonthSelect).val(month);
-    triggerNativeEvent($(this.selectorForMonthSelect)[0], 'change');
-
-    triggerNativeEvent($('td[data-day="' + day + '"] button:visible')[0], selectEvent);
-  },
-  selectedDay: function() {
-    return $('.pika-single td.is-selected button').html();
-  },
-  selectedMonth: function() {
-    return $(this.selectorForMonthSelect + ' option:selected').val();
-  },
-  selectedYear: function() {
-    return $(this.selectorForYearSelect + ' option:selected').val();
-  },
-  minimumYear: function() {
-    return $(this.selectorForYearSelect).children().first().val();
-  },
-  maximumYear: function() {
-    return $(this.selectorForYearSelect).children().last().val();
-  }
+      $(this.selectorForYearSelect).val(year);
+      
+      triggerNativeEvent($(this.selectorForYearSelect)[0], 'change');
+      
+      $(this.selectorForMonthSelect).val(month);
+      
+      triggerNativeEvent($(this.selectorForMonthSelect)[0], 'change');
+      triggerNativeEvent($(element + ' td[data-day="' + day + '"] button:visible')[0], selectEvent);
+    },
+    selectedDay: function() {
+      return $(element + ' .pika-single td.is-selected button').html();
+    },
+    selectedMonth: function() {
+      return $(this.selectorForMonthSelect + ' option:selected').val();
+    },
+    selectedYear: function() {
+      return $(this.selectorForYearSelect + ' option:selected').val();
+    },
+    minimumYear: function() {
+      return $(this.selectorForYearSelect).children().first().val();
+    },
+    maximumYear: function() {
+      return $(this.selectorForYearSelect).children().last().val();
+    }
 };
 
 function triggerNativeEvent(element, eventName) {
   if (document.createEvent) {
     var event = document.createEvent('Events');
+    
     event.initEvent(eventName, true, false);
+    
     element.dispatchEvent(event);
   } else {
     element.fireEvent('on' + eventName);
