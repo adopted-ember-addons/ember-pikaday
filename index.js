e2d7335c31b8ad48f312d8c8f283147e700a6b6b
Josemar Luedke <josemarluedke@gmail.com>
2017-07-01 00:00:04 -0700

    Prepare for FastBoot 1.0 (#157)

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 /* eslint-env node */
 'use strict';
+var fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-pikaday',
   options: {
     nodeAssets: {
       pikaday: {
-        vendor: ['pikaday.js', 'css/pikaday.css']
+        vendor: ['pikaday.js', 'css/pikaday.css'],
+        processTree(input) {
+          return fastbootTransform(input);
+        }
       }
     }
   },
   included() {
     this._super.included.apply(this, arguments);
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      this.import('vendor/pikaday/pikaday.js');
-    }
+
+    this.import('vendor/pikaday/pikaday.js');
     this.import('vendor/pikaday/css/pikaday.css');
   }
 };
