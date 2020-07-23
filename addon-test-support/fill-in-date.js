1e66fa7a56df5b8d8f95d62e02bd2d6488cc7e4a
Christopher Gammie <contact@gammie.co.uk>
2020-07-23 14:10:15 +0100

    Simplify import statement in fillInDate test helper

--- addon-test-support/fill-in-date.js
+++ addon-test-support/fill-in-date.js
@@ -1,9 +1,9 @@
-import * as Interactor from './interactor';
 import { click } from '@ember/test-helpers';
+import { selectDate } from './interactor';
 import close from './close-pikaday';
 
 export default async function fillInDate(selector, date) {
   await click(selector);
-  await Interactor.selectDate(date);
+  await selectDate(date);
   await close();
 }
