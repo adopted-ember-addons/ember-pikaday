bcb018b37c1d93058f51ad9ebb4d429bb2c867d8
Sergey Astapov <sergeastapov@gmail.com>
2024-11-27 22:14:30 -0500

    Template Tag support

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as pikaday } from './modifiers/pikaday';
+export { default as PikadayInput } from './components/pikaday-input';
+export { default as PikadayInputless } from './components/pikaday-inputless';
