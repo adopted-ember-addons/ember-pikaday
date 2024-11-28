2400696034f35e10ef38adf716bed55fdd23fe25
Sergey Astapov <sergeastapov@gmail.com>
2024-11-27 23:10:46 -0500

    Update template-registry.ts

--- src/template-registry.ts
+++ src/template-registry.ts
@@ -2,10 +2,14 @@
 // Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
 // See https://typed-ember.gitbook.io/glint/environments/ember/authoring-addons
 
-// import type MyComponent from './components/my-component';
+import type pikaday from './modifiers/pikaday';
+import type PikadayInput from './components/pikaday-input';
+import type PikadayInputless from './components/pikaday-inputless';
 
-// Remove this once entries have been added! 👇
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export default interface Registry {
-  // MyComponent: typeof MyComponent
+export default interface PikadayRegistry {
+  pikaday: typeof pikaday;
+  'pikaday-input': typeof PikadayInput;
+  PikadayInput: typeof PikadayInput;
+  'pikaday-inputless': typeof PikadayInputless;
+  PikadayInputless: typeof PikadayInputless;
 }
